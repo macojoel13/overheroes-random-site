fd9e7c051f6a78c4075af7a4b65a8dd4245e93d0
Marco Joel <marcojoel25@gmail.com>
2017-12-24 15:42:05 -0400

    Added Tank Heroes and Support Heroes, plus some text fixes.

--- js/scripts.js
+++ js/scripts.js
@@ -59,6 +59,58 @@ function randomHeroPicker(){
         "https://image.ibb.co/bXujfR/Icon_Portrait_Widowmaker.png";
       break;
 
+      case 14: document.getElementById("resultImg").src =
+        "https://image.ibb.co/jwQMC6/Icon_Portrait_Orisa.png";
+      break;
+
+      case 15: document.getElementById("resultImg").src =
+        "https://image.ibb.co/fwSCKm/Icon_Portrait_Reinhardt.png";
+      break;
+
+      case 16: document.getElementById("resultImg").src =
+        "https://image.ibb.co/eKQzzm/Icon_Portrait_Roadhog.png";
+      break;
+
+      case 17: document.getElementById("resultImg").src =
+        "https://image.ibb.co/kccOQR/Icon_Portrait_Winston.png";
+      break;
+
+      case 18: document.getElementById("resultImg").src =
+        "https://image.ibb.co/d9RuX6/Icon_Portrait_Zarya.png";
+      break;
+
+      case 19: document.getElementById("resultImg").src =
+        "https://image.ibb.co/mrYmem/Icon_Portrait_D_va.png";
+      break;
+
+      case 20: document.getElementById("resultImg").src =
+        "https://image.ibb.co/dZuWC6/Icon_Portrait_Moira.png";
+      break;
+
+      case 21: document.getElementById("resultImg").src =
+        "https://image.ibb.co/i8N1C6/Icon_Portrait_Symmetra.png";
+      break;
+
+      case 22: document.getElementById("resultImg").src =
+        "https://image.ibb.co/dmqA5R/Icon_Portrait_Zenyatta.png";
+      break;
+
+      case 23: document.getElementById("resultImg").src =
+        "https://image.ibb.co/k6QMC6/Icon_Portrait_Ana.png";
+      break;
+
+      case 24: document.getElementById("resultImg").src =
+        "https://image.ibb.co/ctN1C6/Icon_Portrait_Lucio.png";
+      break;
+
+      case 25: document.getElementById("resultImg").src =
+        "https://image.ibb.co/kOTV5R/Icon_Portrait_Mercy.png";
+      break;
+
+      case 26: document.getElementById("resultImg").src =
+        "https://image.ibb.co/kSKCkR/Icon_Portrait_Doomfist.png";
+      break;
+
       default:
       break;
     }
